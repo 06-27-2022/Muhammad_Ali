392e2f9143bdbbb7dd7b9ef876fbfdd3186d44f6
aliadison <aliadison@gmail.com>
2022-06-30 17:23:36 -0400

    Summing numbers

--- Summing Numbers/src/com/revature/SummingNumbers.java
+++ Summing Numbers/src/com/revature/SummingNumbers.java
@@ -1,5 +1,19 @@
 package com.revature;
 
 public class SummingNumbers {
-
+	int getSum(int[] arr) {
+		int sum = 0;
+		
+		for(int num: arr) {
+			sum += num;
+		}
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		int[] myArr = {3,7,4};
+		SummingNumbers calculator = new SummingNumbers();
+		
+		System.out.println("Sum: " + calculator.getSum(myArr));
+	}
 }
