f0cfce0bc731df621ddc97cb0c13a1f03c87aeee
aliadison <aliadison@gmail.com>
2022-07-08 16:53:26 -0400

    Finished

--- EvenOrOdd/src/EvenOrOdd.java
+++ EvenOrOdd/src/EvenOrOdd.java
@@ -2,11 +2,14 @@
 public class EvenOrOdd {
 	public static void main(String[] args) {
 		evenOrOdd(23);
-		
+		evenOrOdd(144);
+		evenOrOdd(1);
 	}
 	
-	static void evenOrOdd(int num){
-		System.out.println("Even");
+	static void evenOrOdd(int number){
+		if(number % 2 == 0) 
+			System.out.println("Even");
+		else
+			System.out.println("Odd");	
 	}
-	
 }
